60448cd37f3139b6a49bf80c5a2eccea64f344f5
Majid Ali <majid.ali@codehubsys.com>
2023-06-09 18:08:41 +0500

    Pushed Code

---
+++ ExcelAutomation/Service/IProjectService.cs
@@ -0,0 +1,15 @@
+﻿using ExcelAutomation.Models;
+
+namespace ExcelAutomation.Service
+{
+    public interface IProjectService
+    {
+        void SaveProject(ProjectDto project);
+
+        ICollection<ProjectDto> GetProjects();
+
+        ProjectDto GetProjectById(int  projectId);
+
+        void UpdateProjectDetail(ProjectDto projectDto);
+    }
+}
